60da06a29b5aed5fb640d6233cb32b93dd252a2d
Mikola Lysenko <mikolalysenko@gmail.com>
2013-05-14 12:57:59 -0500

    adding files

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var ndarray = require("ndarray")
+var interp = require("../interp.js")
+
+var x = ndarray.zeros([3, 3])
+x.set(1, 1, 1.0)
+
+for(var u=0.0; u<=3.0; u+=0.25) {
+  var row = []
+  for(var v=0.0; v<=3.0; v+=0.25) {
+    row.push(interp(x, u, v))
+  }
+  console.log(row.join(" "))
+}
